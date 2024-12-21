3011dd60297ece85168b9ebc0b22e1c1132f9abb
Ian Alevy <ian.alevy@gmail.com>
2024-12-21 13:11:21 -0700

    fix order

--- tests/test_time_utilites.py
+++ tests/test_time_utilites.py
@@ -1,9 +1,10 @@
 import unittest
 
+from mtime.time_utilities import add_one
 
 class Test1(unittest.TestCase):
     def test_1(self):
-        assert 4==4
+        assert add_one(3)==4
 
 if __name__ == "__main__":
     unittest.main()
