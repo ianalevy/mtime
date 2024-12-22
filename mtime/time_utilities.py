f2275eb747bcfd73b0e23370109b5f46ea3303dd
Ian Alevy <ian.alevy@gmail.com>
2024-12-21 19:35:03 -0700

    start of time tools

--- mtime/time_utilities.py
+++ mtime/time_utilities.py
@@ -1,2 +1,33 @@
+from __future__ import annotations
+
+import numpy as np
+
+DEFAULT_EPOCH = "19500101"
+
+
+class PTime:
+    def __init__(
+        self,
+        w_sec: str | float | int,
+        f_sec: float,
+        epoch: str = DEFAULT_EPOCH,
+    ):
+        self.w_sec = str(int(w_sec))
+        self.f_sec = f_sec
+        self.epoch = epoch
+
+
+def datetime_to_ptime(np_time: np.datetime64, epoch: str = DEFAULT_EPOCH) -> PTime:
+    year = epoch[:4]
+    month = epoch[4:6]
+    day = epoch[6:8]
+    time_str = f"{year}-{month}-{day}"
+
+    epoch_delta = (np_time - np.datetime64(time_str)) / np.timedelta64(1, "s")
+    f_secs, w_secs = np.modf(epoch_delta)
+
+    return PTime(w_secs, f_secs, epoch)
+
+
 def add_one(number):
     return number + 1
--- test/test_time_utilites.py
+++ test/test_time_utilites.py
@@ -2,14 +2,15 @@ import unittest
 
 import numpy as np
 
-from mtime.time_utilities import PTime
+from mtime.time_utilities import datetime_to_ptime
 
 
 class Test1(unittest.TestCase):
     def test_1(self):
         time_1 = np.datetime64("1950-01-01T01:01:05.123456789")
-        ptime = PTime("65", 0.123456789)
-        assert ptime == time_1
+        res = datetime_to_ptime(time_1)
+        assert res.w_sec == "3665"
+        assert np.isclose(res.f_sec, 0.123456789)
 
 
 if __name__ == "__main__":
